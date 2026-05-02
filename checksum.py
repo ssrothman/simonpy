c97fc5311dbdae909d943de6eda7dec5d4fe11ea
Simon Rothman <srothman@mit.edu>
2026-05-02 13:28:10 -0400

    MD5 checksum - needed because fsspec local filesystem doesn't have a real checksum :(

---
+++ checksum.py
@@ -0,0 +1,14 @@
+
+
+import hashlib
+
+#code from https://stackoverflow.com/questions/3431825/how-to-generate-an-md5-checksum-of-a-file-in-python
+
+def checksum_file(filepath: str) -> str:
+    """Calculate the MD5 checksum of a file."""
+    with open(filepath, "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+
+    return file_hash.hexdigest()
